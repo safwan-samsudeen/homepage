be933b82fcba026c45def52e42b3f8bdf80abf97
Safwan Samsudeen <safwansamsudeen@gmail.com>
2022-10-27 10:55:35 +0530

    improve responsiveness

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 const ROLES = ["Web Developer", "Programmer", "Blogger", "Student", "Reader"];
 const role_n = ROLES.length;
 
-var counter = 0;
+var counter = 1;
 
 function replaceRole() {
+    // Start from the second role, and increment counter
     document.querySelector("#roles").textContent = ROLES[counter];
     counter++;
+    // Reset counter at the last role
     if (counter == role_n) {
         counter = 0;
     }
